d602147ef2da4f9495efcaff1b7147fe38603613
Ali Hamidi <57750952+ahmeroxa@users.noreply.github.com>
2022-04-18 12:32:05 -0700

    update

--- app.go
+++ app.go
@@ -19,7 +19,7 @@ var _ turbine.App = (*App)(nil)
 
 type App struct{}
 
-const NoveltyContext = "testing-1"
+const NoveltyContext = "testing-2"
 
 func (a App) Run(v turbine.Turbine) error {
 	// Identify an upstream data store for your data app
